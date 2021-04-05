c6c120e6b211f01584ad23dd411b33fced707a53
thumbsu <uumj222@gmail.com>
2021-04-05 15:22:28 +0900

    feat: micro container frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import MicroFrontend from "./MicroFrontend";
+
+const {
+  REACT_APP_DOGS_HOST: dogsHost,
+  REACT_APP_CATS_HOST: catsHost
+} = process.env;
+
+function Header() {
+  return (
+    <div className="banner">
+      <h1 className="banner-title">&#128571; Cats and Dogs &#128021;</h1>
+      <h4>Random pics of cats and dogs</h4>
+    </div>
+  );
+}
+
+function Dogs({ history }) {
+  return <MicroFrontend history={history} host={dogsHost} name="Dogs" />;
+}
+
+function Cats({ history }) {
+  return <MicroFrontend history={history} host={catsHost} name="Cats" />;
+}
+
+function GreetingCat({ history }) {
+  return (
+    <div>
+      <Header />
+      <MicroFrontend history={history} host={catsHost} name="Cats" />;
+    </div>
+  );
+}
+
+function Home({ history }) {
+  const [input, setInput] = useState("");
+
+  const handleOnClick = () => {
+    history.push(`/cat/${input}`);
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="home">
+        <input
+          placeholder="Insert a greeting"
+          defaultValue={input}
+          onBlur={e => setInput(e.target.value)}
+        />
+        <button onClick={handleOnClick}>Greet Me</button>
+      </div>
+
+      <div className="home">
+        <div className="content">
+          <div className="cat">
+            <Cats />
+          </div>
+          <div className="dog">
+            <Dogs />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/cat/:greeting" component={GreetingCat} />
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
